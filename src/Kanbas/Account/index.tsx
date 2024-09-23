142630c2bbd0f543aa51218889766fff109d498b
Julian Haurin <julian.haurin@gmail.com>
2024-09-23 11:01:50 -0400

    Initial commit - done with Lab1 and inital stages of A1

---
+++ src/Kanbas/Account/index.tsx
@@ -0,0 +1,31 @@
+
+import { Routes, Route, Navigate } from "react-router";
+
+import AccountNavigation from "./Navigation";
+import Signin from "./Signin";
+import Profile from "./Profile";
+import Signup from "./Signup";
+
+function Account() {
+  return (
+    <div id="wd-account-screen">
+      <table>
+        <tr>
+          <td valign="top">
+            <AccountNavigation />
+          </td>
+          <td valign="top">
+            <Routes>
+              <Route path="/" element={<Navigate to="/Kanbas/Account/Signin" />} />
+              <Route path="/Signin" element={<Signin />} />
+              <Route path="/Profile" element={<Profile />} />
+              <Route path="/Signup" element={<Signup />} />
+            </Routes>
+          </td>
+        </tr>
+      </table>
+    </div>
+  );
+}
+
+export default Account
