b2810a4a5da1c35498c4080cd31054669d7a459c
Julian Haurin <julian.haurin@gmail.com>
2024-10-07 12:35:58 -0400

    Completed Lab2

--- src/Labs/TOC.tsx
+++ src/Labs/TOC.tsx
@@ -1,14 +1,30 @@
 
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
 function TOC() {
+  const { pathname } = useLocation();
   return (
-    <ul>
-      <li><Link to="/Labs">Labs</Link></li>
-      <li><Link to="/Labs/Lab1">Lab 1</Link></li>
-      <li><Link to="/Labs/Lab2">Lab 2</Link></li>
-      <li><Link to="/Labs/Lab3">Lab 3</Link></li>
-      <li><Link to="/Kanbas">Kanbas</Link></li>
+    <ul className="nav nav-pills">
+      <li className="nav-item">
+        <Link to="/Labs" className="nav-link">Labs</Link>
+      </li>
+      <li 
+        className="nav-item"><Link to="/Labs/Lab1" className={`nav-link ${pathname.includes("Lab1") ? "active" : ""}`}>Lab 1</Link>
+      </li>
+      <li 
+        className="nav-item"><Link to="/Labs/Lab2" className={`nav-link ${pathname.includes("Lab2") ? "active" : ""}`}>Lab 2</Link>
+      </li>
+      <li 
+        className="nav-item"><Link to="/Labs/Lab3" className={`nav-link ${pathname.includes("Lab3") ? "active" : ""}`}>Lab 3</Link>
+      </li>
+      <li 
+        className="nav-item"><Link to="/Kanbas" className="nav-link">Kanbas</Link>
+      </li>
+      <li className="nav-item">
+        <Link id="wd-k" to="https://github.com/julianhaurin/kanbas-react-web-app" className="nav-link">
+          My GitHub
+        </Link>
+      </li>
     </ul>
   );
 }
